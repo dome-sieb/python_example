afe1f4f673e7152650608f0f839027dda39bcdad
dome-sieb <sieberdominique10@gmail.com>
2024-07-24 13:08:41 +0200

    Geburtstagskarten Generator

---
+++ Geburtstagskarten_Generator.py
@@ -0,0 +1,13 @@
+
+print("---Willkommen zum Geburtstagskarten-Generator---")
+
+name = input("Name der zu gratulierenden Person:")
+age = input("Alter der zu gratulierenden Person:")
+sender = input("Name des Absenders:")
+
+print("Hallo" + name + ",")
+print("Ich wünsche dir alles Gute zu deinem " + age + "ten Geburtstag.")
+print("Ich hoffe du hast einen tollen Tag und mit deinen liebstem Feierst.")
+print("Zudem wuensche ich die alles Gute und viel Erfolg im neuen Lebensjahr.")
+print("Viele Grüße,")
+print(sender)
---
+++ main.py
@@ -0,0 +1,55 @@
+print("Hallo Welt ich lerne gerade programmieren")
+print("Hallo Welt ich lerne gerade programmieren")
+# das ist unser Hallo Welt programm ;)
+#Ich muss kacken :D
+#ich war Kacken :D
+#Ganzzahlrechnung
+print(15+5)
+print(15-5)
+print(15*5)
+print(15/5)
+print(15+7*4/2)
+print((15+7)*(4/2))
+#Kommazahlenrechnung
+#Komma wird als Punkt dargestellt!!!
+print(2.1+1.5)
+print(5.89-1.4)
+print(5.89*1.28)
+print(5.89/1.24)
+
+#Strings
+print("Hallo Welt")
+print('hallo welt')
+print('Programmiersprache Python ist cool')
+
+#Variablen
+#Sprechend bennen am besten in Englisch
+age = 28
+age = int(age)
+print(age)
+name = "Dome"
+print(name)
+# Datentypen
+#Verkettung
+print("Hallo Welt"+ "Du bist doof")
+int_variable = 100 #Ganzzahlen!!!
+print(int_variable)
+float_variable = 3.14 # Kommazahlen!!!
+print(float_variable)
+
+
+#Input Funktion
+
+print("Anfang des Programmes")
+input()
+print("Ende des Programmes")
+
+print("Willkommen zum Addierer- Programm")
+value1 = input("Erste Zahl eingeben:")
+value2 = input("Zweite Zahl eingeben:")
+
+
+print(int(value1)+int(value2))
+
+
+
